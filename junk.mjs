9070d0cc1165a79042d8d7cfcafdae54b55f8b59
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2024-07-06 10:23:45 -0700

    experimint 2

--- junk.mjs
+++ junk.mjs
@@ -1,13 +1,10 @@
-import { Croquet, requestAnimationFrame, serializePromises, delay } from './utilities.mjs';
+import { Croquet, join } from './administrivia.mjs';
 export { Croquet };
 
 class M extends Croquet.Model {}
 class V extends Croquet.View {}
 M.register(M.name)
-console.log(await Croquet.Session.join({
-  appId: 'com.gmail.howard.stearns.microverse',
-  apiKey: '17GxHzdAvd4INCAHfJoDm39LH6FkmkLa5qdLhGLqA',
-  password: "secret",
+console.log(await join({
   name: 'Controller11',
   step: 'manual',
   model: M,
