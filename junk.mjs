09395e89484bf9624e98f7ed4c9874d8f5e0919b
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2024-07-06 10:36:20 -0700

    experiment 4

--- junk.mjs
+++ junk.mjs
@@ -1,3 +1,12 @@
-import { player, PlayerView } from './player.mjs';
+import { player, PlayerView, Croquet, join } from './player.mjs';
 
-console.log(await player('Controller11'));
+class M extends Croquet.Model {}
+class V extends Croquet.View {}
+M.register(M.name)
+console.log(await join({
+  name: 'Controller11',
+  step: 'manual',
+  model: M,
+  view: V
+}));
+//console.log(await player('Controller11'b));
